f0cf4f4f35b693e0de84a1435dfc01d24c26a1a8
babu-lang <danlindo2007@gmail.com>
2025-06-16 12:56:55 -0300

    midi.s

--- codigos-testes/midi.s
+++ codigos-testes/midi.s
@@ -1,32 +1,27 @@
-###############################################
-#  Programa de exemplo para Syscall MIDI      #
-#  ISC Abr 2018				      #
-#  Marcus Vinicius Lamar		      #
-###############################################
 
 .data
 # Numero de Notas a tocar
 NUM: .word 18
-# lista de nota,duraÁ„o,nota,duraÁ„o,nota,duraÁ„o,...
+# lista de nota,dura√ß√£o,nota,dura√ß√£o,nota,dura√ß√£o,...
 NOTAS: 77,368,71,368,72,368,65,184,67,920,60,368,62,184,67,2760,60,184,62,184,62,368,65,368,67,368,72,184,62,920,72,368,62,184,67,1656
 
 .text
-	la s0,NUM		# define o endereÁo do n˙mero de notas
+	la s0,NUM		# define o endere√ßo do n√∫mero de notas
 	lw s1,0(s0)		# le o numero de notas
-	la s0,NOTAS		# define o endereÁo das notas
+	la s0,NOTAS		# define o endere√ßo das notas
 	li t0,0			# zera o contador de notas
 	li a2,0			# define o instrumento
 	li a3,127		# define o volume
 
-LOOP:	beq t0,s1, FIM		# contador chegou no final? ent„o  v· para FIM
+LOOP:	beq t0,s1, FIM		# contador chegou no final? ent√£o  v√° para FIM
 	lw a0,0(s0)		# le o valor da nota
 	lw a1,4(s0)		# le a duracao da nota
 	li a7,31		# define a chamada de syscall
 	ecall			# toca a nota
-	mv a0,a1		# passa a duraÁ„o da nota para a pausa
+	mv a0,a1		# passa a dura√ß√£o da nota para a pausa
 	li a7,32		# define a chamada de syscal 
 	ecall			# realiza uma pausa de a0 ms
-	addi s0,s0,8		# incrementa para o endereÁo da prÛxima nota
+	addi s0,s0,8		# incrementa para o endere√ßo da pr√≥xima nota
 	addi t0,t0,1		# incrementa o contador de notas
 	j LOOP			# volta ao loop
 	
